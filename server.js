2b0a4b035f3fbfe51513c88b62215c2ae95b09f4
Mustard <Lance.Mustard@WorleyParsons.com>
2017-09-06 16:10:39 +0800

    leason 48

--- server.js
+++ server.js
@@ -20,9 +20,9 @@ app.use((request, response, next) => {
     next();
 });
 
-app.use((request, response, next) => {
-    response.render('maintenance.hbs');
-});
+// app.use((request, response, next) => {
+//     response.render('maintenance.hbs');
+// });
 
 app.use(express.static(__dirname + '/public'));
 
@@ -44,6 +44,12 @@ app.get('/', (request, response) => {
 app.get('/about', (request, response) => {
     response.render('about.hbs', {
         pageTitle: 'About Page'
+    });
+});
+
+app.get('/projects', (request, response) => {
+    response.render('projects.hbs', {
+        pageTitle: 'Projects Page'
     });
 });
 
